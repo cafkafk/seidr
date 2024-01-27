39e7c02d55a63fc8d943ab831076ff56dd9dc5ad
Christina Sørensen <christina@cafkafk.com>
2024-01-27 15:52:17 +0100

    build(flake): change flake inputs
    Signed-off-by: Christina Sørensen <christina@cafkafk.com>

--- flake.nix
+++ flake.nix
@@ -6,29 +6,29 @@
   description = "Hon hafði um sik hnjóskulinda, ok var þar á skjóðupungr mikill, ok varðveitti hon þar í töfr sín, þau er hon þurfti til fróðleiks at hafa.";
 
   inputs = {
-    nixpkgs.url = "http:/rime.cx/v1/github/NixOS/nixpkgs/b/nixpkgs-unstable.tar.gz";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
 
     flake-utils = {
-      url = "http://rime.cx/v1/github/semnix/flake-utils.tar.gz";
+      url = "github:semnix/flake-utils";
     };
 
     naersk = {
-      url = "http://rime.cx/v1/github/semnix/naersk.tar.gz";
+      url = "github:semnix/naersk";
       inputs.nixpkgs.follows = "nixpkgs";
     };
 
     rust-overlay = {
-      url = "http://rime.cx/v1/github/semnix/rust-overlay.tar.gz";
+      url = "github:semnix/rust-overlay";
       inputs.nixpkgs.follows = "nixpkgs";
     };
 
     treefmt-nix = {
-      url = "http://rime.cx/v1/github/semnix/treefmt-nix.tar.gz";
+      url = "github:semnix/treefmt-nix";
       inputs.nixpkgs.follows = "nixpkgs";
     };
 
     powertest = {
-      url = "http://rime.cx/v1/github/eza-community/powertest.tar.gz";
+      url = "github:eza-community/powertest";
       inputs = {
         nixpkgs.follows = "nixpkgs";
         naersk.follows = "naersk";
@@ -38,7 +38,7 @@
     };
 
     pre-commit-hooks = {
-      url = "http://rime.cx/v1/github/semnix/pre-commit-hooks.nix.tar.gz";
+      url = "github:semnix/pre-commit-hooks.nix";
       inputs.nixpkgs.follows = "nixpkgs";
       inputs.flake-utils.follows = "flake-utils";
     };
